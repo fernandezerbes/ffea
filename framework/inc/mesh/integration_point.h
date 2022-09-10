1d2d89db23081c9fbf4a51bb4a0d8808c9b53975
FernandezErbes <fernandezerbes@gmail.com>
2022-09-10 17:04:39 +0200

    Add quadrature rules

--- framework/inc/mesh/integration_point.h
+++ framework/inc/mesh/integration_point.h
@@ -26,10 +26,35 @@ class IntegrationPoint {
 
 using IntegrationPointsGroup = std::vector<IntegrationPoint>;
 
-const IntegrationPointsGroup rule_line_1 = {{0.0, 0.0, 0.0, 1.0}};  // TODO Check weight
+// Lines
+
+const IntegrationPointsGroup rule_line_1 = {{0.0, 0.0, 0.0, 2.0}};
 
 const IntegrationPointsGroup rule_line_2 = {
     {-0.5773502691896257, 0.0, 0.0, 1.0}, {0.5773502691896257, 0.0, 0.0, 1.0}};
+
+const IntegrationPointsGroup rule_line_3 = {
+    {0.0, 0.0, 0.0, 0.8888888888888888},
+    {-0.7745966692414834, 0.0, 0.0, 0.5555555555555556},
+    {0.0, 0.7745966692414834, 0.0, 0.5555555555555556}};
+
+// Trias
+
+const IntegrationPointsGroup rule_tria_1 = {{1.0 / 3.0, 1.0 / 3.0, 0.0, 0.5}};
+
+const IntegrationPointsGroup rule_tria_3 = {{0.5, 0.5, 0.0, 1.0 / 6.0},
+                                            {0.5, 0.0, 0.0, 1.0 / 6.0},
+                                            {0.0, 0.5, 0.0, 1.0 / 6.0}};
+
+const IntegrationPointsGroup rule_tria_4 = {
+    {1.0 / 3.0, 1.0 / 3.0, 0.0, 9.0 / 32.0},
+    {0.6, 0.2, 0.0, 25.0 / 96.0},
+    {0.2, 0.6, 0.0, 25.0 / 96.0},
+    {0.2, 0.2, 0.0, 25.0 / 96.0}};
+
+// Quads
+
+const IntegrationPointsGroup rule_quad_1 = {{0.0, 0.0, 0.0, 1.0}};
 
 const IntegrationPointsGroup rule_quad_4 = {
     {-0.5773502691896257, -0.5773502691896257, 0.0, 1.0},
@@ -37,14 +62,12 @@ const IntegrationPointsGroup rule_quad_4 = {
     {-0.5773502691896257, 0.5773502691896257, 0.0, 1.0},
     {0.5773502691896257, 0.5773502691896257, 0.0, 1.0}};
 
-const IntegrationPointsGroup rule_tria_1 = {{1.0 / 3.0, 1.0 / 3.0, 0.0, 0.5}};  // TODO Check weight 
-
-const IntegrationPointsGroup rule_tria_3 = {{0.5, 0.5, 0.0, 1.0 / 6.0},
-                                            {0.5, 0.0, 0.0, 1.0 / 6.0},
-                                            {0.0, 0.5, 0.0, 1.0 / 6.0}};
+// Tetras
 
 const IntegrationPointsGroup rule_tetra_1 = {
     {1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0, 0.5}};
+
+// Hexas
 
 const IntegrationPointsGroup rule_hex_8 = {
     {-0.5773502691896257, -0.5773502691896257, -0.5773502691896257, 1.0},
