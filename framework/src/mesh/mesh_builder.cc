8954af68d172e61a1d740a5b469b2aa6377836b2
FernandezErbes <fernandezerbes@gmail.com>
2022-09-11 12:09:13 +0200

    Print dofs

--- framework/src/mesh/mesh_builder.cc
+++ framework/src/mesh/mesh_builder.cc
@@ -12,7 +12,7 @@ MeshBuilder::~MeshBuilder() {}
 
 Mesh MeshBuilder::Build(size_t number_of_fields) const {
   Mesh mesh(geometry_, number_of_fields);
-  
+
   for (auto &item : geometry_.geometric_entities_groups_) {
     const auto &group_name = item.first;
     const auto &element_factory = element_factories_.at(group_name);
@@ -21,13 +21,17 @@ Mesh MeshBuilder::Build(size_t number_of_fields) const {
     }
   }
 
+  std::cout << "Built mesh with " << mesh.number_of_dofs() << " dofs"
+            << std::endl;
+
   return mesh;
 }
 
 void MeshBuilder::RegisterElementFactory(const std::string &group_name,
                                          const ElementFactory &factory) {
   if (element_factories_.contains(group_name)) {
-    throw std::runtime_error("Factory already registered for group " + group_name);
+    throw std::runtime_error("Factory already registered for group " +
+                             group_name);
   }
 
   element_factories_.insert({group_name, factory});
