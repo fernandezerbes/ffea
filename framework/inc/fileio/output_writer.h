977a25dfbd8a001e895565cf72a00eeb036fee83
FernandezErbes <fernandezerbes@gmail.com>
2023-03-15 20:02:56 +0100

    Remove clang-formatting to vtk cell map

--- framework/inc/fileio/output_writer.h
+++ framework/inc/fileio/output_writer.h
@@ -25,13 +25,21 @@ class OutputWriter {
 
 vtu11::VtkIndexType MapToVtkNodeIdx(GeometricEntityType entity_type, size_t node_idx);
 
+// clang-format off
 const std::unordered_map<GeometricEntityType, vtu11::VtkCellType> geometric_entity_to_vtk_cell_map =
-    {{GeometricEntityType::kTwoNodeLine, 3},      {GeometricEntityType::kThreeNodeTria, 5},
-     {GeometricEntityType::kFourNodeQuad, 8},     {GeometricEntityType::kFourNodeTetra, 10},
-     {GeometricEntityType::kEightNodeHex, 12},    {GeometricEntityType::kSixNodePrism, 13},
-     {GeometricEntityType::kFiveNodePiramid, 14}, {GeometricEntityType::kThreeNodeLine, 21},
-     {GeometricEntityType::kSixNodeTria, 22},     {GeometricEntityType::kNineNodeQuad, 23},
-     {GeometricEntityType::kTenNodeTetra, 24},    {GeometricEntityType::kTwentySevenNodeHex, 25}};
+    {{GeometricEntityType::kTwoNodeLine, 3},
+     {GeometricEntityType::kThreeNodeTria, 5},
+     {GeometricEntityType::kFourNodeQuad, 8},
+     {GeometricEntityType::kFourNodeTetra, 10},
+     {GeometricEntityType::kEightNodeHex, 12},
+     {GeometricEntityType::kSixNodePrism, 13},
+     {GeometricEntityType::kFiveNodePiramid, 14},
+     {GeometricEntityType::kThreeNodeLine, 21},
+     {GeometricEntityType::kSixNodeTria, 22},
+     {GeometricEntityType::kNineNodeQuad, 23},
+     {GeometricEntityType::kTenNodeTetra, 24},
+     {GeometricEntityType::kTwentySevenNodeHex, 25}};
+// clang-format on
 
 }  // namespace ffea
 
